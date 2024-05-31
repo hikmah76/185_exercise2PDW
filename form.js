12f5bcb0792539e4b05bd6189a03572441b56d61
nur hikmah <nur.hikmah190504@gmail.com>
2024-06-01 00:17:57 +0700

    first commit

---
+++ form.js
@@ -0,0 +1,31 @@
+function kirimData() {
+    var nama = document.getElementById("nama").value;
+    var laguFavorite = document.getElementById("lagu_favorite").value;
+    var penyanyiBandFavorite = document.getElementById("penyanyi_band_favorite").value;
+    var tanggal = document.getElementById("tanggal").value; // Added this line to get the date value
+    var peminatan = document.querySelector('input[name="Platform"]:checked').value;
+    var alasanMemilihLagu = document.getElementById("alasan_memilih_lagu_ini").value;
+
+    alert(`
+        Nama: ${nama}
+        Lagu Favorite: ${laguFavorite}
+        Penyanyi/Band Favorite: ${penyanyiBandFavorite}
+        Tanggal: ${tanggal}  // Included the date in the alert
+        Platform Musik Favorit: ${peminatan}
+        Alasan Memilih Lagu Ini: ${alasanMemilihLagu}
+    `);
+}
+
+function submitForm() {
+    var form = document.getElementById("registrationForm");
+    var formData = new FormData(form);
+
+    document.getElementById("output").innerHTML = "";
+
+    var output = "<h2>Informasi yang Anda isi:</h2>";
+    formData.forEach(function(value, key) {
+        output += "<p><strong>" + key + ":</strong> " + value + "</p>";
+    });
+
+    document.getElementById("output").innerHTML = output;
+}
---
+++ script.js
@@ -0,0 +1,26 @@
+
+video.addEventListener("click", function video(){
+    
+    alert("ini video Red Velvet");
+  });
+  
+  
+  audio.addEventListener("click", function audio(){
+    
+    alert("ini Audio Red Velvet");
+  });
+  
+  
+  gambar.addEventListener("click", function gambar(){
+    
+    alert("ini gambar Red Velvet");
+  });
+  
+  function kirimData() {
+    // Mengarahkan ke halaman from.html
+    window.location.href = "from.html";
+  }
+
+  
+
+
